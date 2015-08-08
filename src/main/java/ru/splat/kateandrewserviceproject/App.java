64ffb63b66347b1282be470fff2991a7d06f7ae7
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-08-08 19:12:03 +0300

    initial commit

---
+++ src/main/java/ru/splat/kateandrewserviceproject/App.java
@@ -0,0 +1,13 @@
+package ru.splat.kateandrewserviceproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
