32ebc1ec7a13203b50a5d13c7621188e396b0860
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-08-08 23:20:55 +0300

    add ServerSocket and exemplary version of function init which read properties

--- src/main/java/ru/splat/kateandrewserviceproject/ProviderJSON.java
+++ src/main/java/ru/splat/kateandrewserviceproject/ProviderJSON.java
@@ -1,5 +1,9 @@
 package ru.splat.kateandrewserviceproject;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * <p>
  * @author andrey
@@ -7,13 +11,45 @@ package ru.splat.kateandrewserviceproject;
  * Possible only one connection at a time
  */
 public class ProviderJSON {
+	private final static int DEFAULT_PORT = 12345;
+	
+	private String protocol;
+	private long periodWaitTime = 1000L;
+	private int idList[];
+	
+	private static void init() {
+		/**
+		 * properties
+		 * protocol = string: xml or json
+		 * periodWaitTime: T is random in (0, waitingTime]
+		 * idList
+		 */
+		
+		
+	}
 	
 	/**
 	 * main method to start a Provider 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		try {
+			ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
+			try {
+				while (true) {
+					Socket socket = serverSocket.accept();
+					try {
+						// xml or json
+					} finally {
+						socket.close();
+					}
+				}
+			} finally {
+				serverSocket.close();
+			}
+		} catch (IOException e) {
+			//
+		}
 		
 	}
 }
