e3e7c02356c0f789e2b90b32cacb9e059684c732
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-08-08 20:04:43 +0300

    add init version of ProviderJSON

---
+++ src/main/java/ru/splat/kateandrewserviceproject/ProviderJSON.java
@@ -0,0 +1,19 @@
+package ru.splat.kateandrewserviceproject;
+
+/**
+ * <p>
+ * @author andrey
+ * Class provider of transmitting data to the server through socket
+ * Possible only one connection at a time
+ */
+public class ProviderJSON {
+	
+	/**
+	 * main method to start a Provider 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+}
