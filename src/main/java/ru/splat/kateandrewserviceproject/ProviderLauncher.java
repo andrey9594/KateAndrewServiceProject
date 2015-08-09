0d80b625aa47d947c01eada96f303ac2a112e513
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-08-09 13:42:02 +0300

    add class for launching several Providers

---
+++ src/main/java/ru/splat/kateandrewserviceproject/ProviderLauncher.java
@@ -0,0 +1,17 @@
+package ru.splat.kateandrewserviceproject;
+
+/**
+ * <p>
+ * Class for launching several Providers
+ * @author andrey
+ */
+public class ProviderLauncher {
+	/**
+	 * <p>
+	 * Main method for launching several Providers
+	 * @param args 
+	 */
+	public static void main(String[] args) {
+		Provider provider = new Provider();
+	}
+}
