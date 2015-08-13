f8036706c9a18a8b1e8857521d588764a0688496
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-08-13 16:38:58 +0300

    added logging

--- Provider/src/main/java/ru/splat/kateandrewserviceproject/ProviderLauncher.java
+++ Provider/src/main/java/ru/splat/kateandrewserviceproject/ProviderLauncher.java
@@ -1,20 +1,39 @@
 package ru.splat.kateandrewserviceproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * <p>
  * Class for launching several Providers
  * @author andrey
  */
 public class ProviderLauncher {
+	static private final Logger log = LoggerFactory.getLogger(ProviderLauncher.class);
+	
 	/**
 	 * <p>
 	 * Main method for launching several Providers
 	 * @param args 
 	 */
 	public static void main(String[] args) {
-	//	Provider provider = new Provider("resources/config_xml.properties");
-		Provider provider = new Provider("resources/config_json.properties");
-
-		provider.start(); 
+		/*
+		log.info("Starting xml provider...");	
+		new Thread() {
+			public void run() {
+				Provider xmlProvider = new Provider("resources/config_xml.properties");
+				xmlProvider.start();
+			}
+		}.start();
+		log.info("Xml provider has been started");
+		*/
+		log.info("Starting xml provider...");
+		new Thread() {
+			public void run() {
+				Provider jsonProvider = new Provider("resources/config_json.properties");
+				jsonProvider.start();
+			}
+		}.start();
+		log.info("Json provider has been started");		
 	}
 }
