eac6c7bd5f8cd02a1d403f0bc959c817d47d1de9
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-08-17 12:45:50 +0300

    add simple SWT shell

--- DesktopClient/src/main/java/ru/splat/DesktopClient/ClientLauncher.java
+++ DesktopClient/src/main/java/ru/splat/DesktopClient/ClientLauncher.java
@@ -1,11 +1,40 @@
 package ru.splat.DesktopClient;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
- *
+ * <p>
+ * Main class for launching a Desktop Client
  */
 public class ClientLauncher {
+	static private final Logger log = LoggerFactory.getLogger(ClientLauncher.class);
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+        Display display = new Display(); 
+        log.info("Display was created");
+        
+        Shell shell = new Shell(display);
+		shell.addListener(SWT.KeyDown, new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				if (event.type == SWT.KeyDown && event.character == SWT.ESC) {
+					shell.dispose();
+				}
+			}
+		});
+		
+        shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
+		log.info("Display was disposed");
 	}
 }
