fd3e18502e7740b8f81187ead2b420cc084c819f
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-08-17 10:25:09 +0300

    DesktopClient initial commit

---
+++ DesktopClient/src/main/java/ru/splat/DesktopClient/ClientLauncher.java
@@ -0,0 +1,11 @@
+package ru.splat.DesktopClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class ClientLauncher {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
