b9c3911bb0dea58df5068439519d9d87606b41ad
Katena <katrina-yakovleva@yandex.ru>
2015-09-03 18:24:57 +0300

    Saved and updated dates in GUAVA structure

--- DesktopClient/src/main/java/ru/splat/DesktopClient/Client.java
+++ DesktopClient/src/main/java/ru/splat/DesktopClient/Client.java
@@ -1,13 +1,16 @@
 package ru.splat.DesktopClient;
 
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
+import com.google.common.collect.*;
 import com.google.gson.Gson;
 import com.rabbitmq.client.*;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.widgets.Table;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.splat.DesktopClient.listeners.GraphListener;
@@ -18,20 +21,19 @@ import ru.splat.DesktopClient.listeners.XmlProviderListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Map;
+import java.sql.Timestamp;
+
 import java.util.Properties;
 import java.util.concurrent.TimeoutException;
 
-import static com.google.common.collect.Maps.newHashMap;
-
 
 /**
- * <p>
+ * <p/>
  *
  * @author Ekaterina
  *         Desktop client
- *         Take data from Service(by RabbitMQ) and make desktop window
- *         in which output "id" and "value" in two forms(table and graph)
+ *         Take data from Service(by RabbitMQ) and make desktop window in which displays
+ *         the history of changes in the state of object in two forms(table and graph)
  */
 
 public class Client
@@ -49,14 +51,18 @@ public class Client
 
     private static volatile Client instance;
 
-    Multiset<Integer> values = HashMultiset.create();
+    public com.google.common.collect.Table<Integer, Timestamp, Integer> weightedGraphXml = TreeBasedTable.create();
 
-    Map<Integer, Multiset<Integer>> myMap = newHashMap();
+    public com.google.common.collect.Table<Integer, Timestamp, Integer> weightedGraphJson = TreeBasedTable.create();
 
-    private int providerId = 0; // 0 - xml; 1 - json
+    public int providerId = 0; // 0 - xml; 1 - json
+
+    public Table table;
+
+    private static Text text;
 
 
-    Client()
+    public Client()
     {
     }
 
@@ -125,8 +131,10 @@ public class Client
             private Gson gson = new Gson();
 
 
-            @Override public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
-                    byte[] body) throws IOException
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                    AMQP.BasicProperties properties, byte[] body)
+                    throws IOException
             {
                 String jsonString = new String(body, "UTF-8");
                 ProviderPackage providerPackage = gson.fromJson(jsonString, ProviderPackage.class);
@@ -141,7 +149,7 @@ public class Client
     /**
      * Updates data, when new packege come
      *
-     * @param providerPackage
+     * @param providerPackage latest packege, which we receved.
      */
     private void processPackage(ProviderPackage providerPackage)
     {
@@ -151,15 +159,43 @@ public class Client
 
 
     /**
-     * Update data in hashmap
+     * Update data in Table(Guava structure)
      *
-     * @param id_new       id
-     * @param value_new    value
-     * @param provider_new provider name ("providerxml" or "providerjson")
+     * @param idNew       id  identifier of Object, which we received
+     * @param valueNew    value positive or negative value, which must be added to Object history
+     * @param providerNew provider name ("providerxml" if data received from XMl provider or
+     *                    "providerjson" if data received from Json provider)
      */
-    private void updateData(int id_new, int value_new, String provider_new)
+    private void updateData(int idNew, int valueNew, String providerNew)
     {
-        //Todo: update myMap and values;
+        if (providerNew.equals("providerxml"))
+        {
+            if (weightedGraphXml.containsRow(idNew))
+            {
+                weightedGraphXml.rowMap();
+                weightedGraphXml.row(idNew).put(new java.sql.Timestamp(new java.util.Date().getTime()), valueNew);
+                log.debug("Table 'weightedGraphXml' with id = {} is updated", idNew);
+            }
+            else
+            {
+                weightedGraphXml.put(idNew, new java.sql.Timestamp(new java.util.Date().getTime()), valueNew);
+                log.debug("New record  with id = {} is created in Table 'weightedGraphXml'", idNew);
+            }
+        }
+        else if (providerNew.equals("providerjson"))
+        {
+            if (weightedGraphJson.containsRow(idNew))
+            {
+                weightedGraphJson.rowMap();
+                weightedGraphJson.row(idNew).put(new java.sql.Timestamp(new java.util.Date().getTime()), valueNew);
+                log.debug("Table 'weightedGraphJson' with id = {} is updated", idNew);
+            }
+            else
+            {
+                weightedGraphJson.put(idNew, new java.sql.Timestamp(new java.util.Date().getTime()), valueNew);
+                log.debug("New record  with id = {} is created in Table 'weightedGraphJson'", idNew);
+            }
+        }
     }
 
 
@@ -173,11 +209,17 @@ public class Client
         log.info("Display was created");
 
         Shell shlDesktopClient = new Shell(display);
+        Image bgImg = new Image(display, "resources/BackgroundImage.jpg");
         shlDesktopClient.setText("Desktop Client on SWT");
-        shlDesktopClient.setLayout(new GridLayout());
+        GridLayout gl_shlDesktopClient = new GridLayout();
+        shlDesktopClient.setLayout(gl_shlDesktopClient);
+        Image icon = new Image(display, "resources/icon.jpg");
+        shlDesktopClient.setImage(icon);
+        shlDesktopClient.setBackgroundImage(bgImg);
+        shlDesktopClient.setBackgroundMode(SWT.INHERIT_FORCE);
 
-        Label lblXmlprovider = new Label(shlDesktopClient, SWT.NONE);
-        lblXmlprovider.setText(" ");
+        Label lblprovider = new Label(shlDesktopClient, SWT.NONE);
+        lblprovider.setText("");
 
         Menu menu = new Menu(shlDesktopClient, SWT.BAR);
         shlDesktopClient.setMenuBar(menu);
@@ -206,10 +248,10 @@ public class Client
         MenuItem mGraph = new MenuItem(menu_View, SWT.NONE);
         mGraph.setText("Graph");
 
-        mGraph.addSelectionListener(new GraphListener(shlDesktopClient, providerId));
-        mTable.addSelectionListener(new TableListener(shlDesktopClient, providerId));
-        mXmlProvider.addSelectionListener(new XmlProviderListener(shlDesktopClient, lblXmlprovider, providerId));
-        mJsonProvider.addSelectionListener(new JsonProviderListener(shlDesktopClient, lblXmlprovider, providerId));
+        mGraph.addSelectionListener(new GraphListener(shlDesktopClient, providerId, this));
+        mTable.addSelectionListener(new TableListener(shlDesktopClient, providerId, this));
+        mXmlProvider.addSelectionListener(new XmlProviderListener(shlDesktopClient, lblprovider, this));
+        mJsonProvider.addSelectionListener(new JsonProviderListener(shlDesktopClient, lblprovider, this));
 
         shlDesktopClient.open();
         log.info("Start window have been drawn");
