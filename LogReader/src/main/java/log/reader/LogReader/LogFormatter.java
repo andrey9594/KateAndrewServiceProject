699abc06430a962538f25c6cd7d792206d364df6
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-09-20 11:30:52 +0300

    changed hierarchy

---
+++ LogReader/src/main/java/log/reader/LogReader/LogFormatter.java
@@ -0,0 +1,20 @@
+package log.reader.LogReader;
+
+public class LogFormatter {	
+	private String logFilePath;
+	
+	public LogFormatter(String logFilePath) {
+		this.logFilePath = logFilePath;
+	}
+	
+	public String nextXML() {
+		String xml = "";
+		
+		return xml;
+	}
+	
+	private String nextLine() {
+		return "";	
+	}
+	
+}
