f88e696456448f4b68803fbcf7ecbf2ca3162eb2
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-09-21 17:39:29 +0300

    added JAXB unmarshalling xml string which only have tag event_list; some javadoc added

--- LogReader/src/main/java/log/reader/LogReader/Main.java
+++ LogReader/src/main/java/log/reader/LogReader/Main.java
@@ -6,23 +6,37 @@ import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.StringBufferInputStream;
 import java.util.Properties;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import generated.EventList;
+
 /**
- * Hello world!
- *
+ * Main class for getting information from log files
  */
 public class Main {
 	static private final Logger log = LoggerFactory.getLogger(Main.class);
 	
-	static private String pathToLogs;
-	static private String logFormat;
 	static private final String pathToProperties = "resources/config.properties";
 	
+	static private String pathToLogs;
+	static private String logFormat;
+
+	/**
+	 * Main method for getting events from log files
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		/**
+		 * loading properties from pathToProperties
+		 */
         Properties properties = new Properties();
         log.info("Loading configuration from {}...", pathToProperties);
 		try {
@@ -34,11 +48,14 @@ public class Main {
 			log.error("Can't read config file {}", pathToProperties);
 			e.printStackTrace();
 		}
-		pathToLogs = properties.getProperty("path_to_logs");
-		logFormat = properties.getProperty("log_format");
+		pathToLogs = properties.getProperty("path_to_logs"); // where log files are
+		logFormat = properties.getProperty("log_format"); // .log or another
 		log.info("Configuration from {} has been successfully loaded", pathToProperties);
         
-		//список файлов
+		/**
+		 * Scanning directory and finding all log files
+		 * with extension: *.logFormat
+		 */
 		File logsDirectory = new File(pathToLogs);
 		File[] logFilesList = logsDirectory.listFiles(new FileFilter() {
 			@Override
@@ -54,18 +71,39 @@ public class Main {
 			log.error("Log files was't found in {1}!", pathToLogs);
 			throw new NullPointerException();
 		}
+		
 		for (int i = 0; i < logFilesList.length; i++) {
 			try {
-				log.info("Proccessing {} started", logFilesList[i]);
+				log.info("Processing {} started", logFilesList[i]);
 				LogFormatter formatter = new LogFormatter(logFilesList[i]);
-				
+				/**
+				 * until the end of current log file not reached
+				 */
 				while (true) {
 					try {
 						String xml = formatter.nextXML();
 						log.debug("Received xml from log file {}:\n {}", logFilesList[i], xml);
-						// ???
+						/**
+						 * xml starting with <tag attributes> or <tag>
+						 */
+						StringBuilder s = new StringBuilder("");
+						for (int j = 0; j < xml.length() && xml.charAt(j) != ' ' && xml.charAt(j) != '>'; j++)
+							s.append(xml.charAt(j));
+						/**
+						 * we only interested in event_list tag
+						 */
+						if (s.toString().equals("<event_list")) {
+							 try {
+								JAXBContext jaxbContext = JAXBContext.newInstance(EventList.class);
+								Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+								EventList eventList = (EventList)unmarshaller.unmarshal(new StringBufferInputStream(xml));
+							} catch (JAXBException e) {
+								log.error("Problem with JAXB", e);
+								e.printStackTrace();
+							}
+						}
 					} catch (EOFException e) {
-						log.info("Proccessing {} completed", logFilesList[i]);
+						log.info("Processing {} completed. {}/{} done.", logFilesList[i], (i + 1), logFilesList.length);
 						break;
 					} catch (IOException e) {
 						log.error("Can't read log file {}!", logFilesList[i], e);
@@ -77,6 +115,6 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
-
+		
 	}
 }
