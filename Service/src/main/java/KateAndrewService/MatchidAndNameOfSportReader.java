0cfaacb91ced48e9c77c15a9bfc73bdcd9dfa659
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-10-20 21:21:51 +0300

    added class for parsing cvs file with map matchid -> sportType

---
+++ Service/src/main/java/KateAndrewService/MatchidAndNameOfSportReader.java
@@ -0,0 +1,59 @@
+package KateAndrewService;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import matchstatistic.MatchType;
+
+public class MatchidAndNameOfSportReader {
+	private Scanner scanner;
+	
+	public MatchidAndNameOfSportReader(String pathToFile) throws FileNotFoundException {
+		scanner = new Scanner(new File(pathToFile));
+	}
+	
+	public Map <Integer, MatchType> getSportNameForMatchidMap() {
+		Map <Integer, MatchType> sportNameForMatchidMap = new TreeMap<>();
+		
+		while (scanner.hasNext()) {
+			String line = scanner.nextLine();
+			
+			String words[] = line.split(",");
+			
+			int id = Integer.parseInt(words[0]);
+			words[1] = words[1].substring(1);
+			words[1] = words[1].substring(0, words[1].length() - 1);	
+		
+			MatchType sportName = null;
+			switch(words[1]) {
+			case "Soccer":
+				sportName = MatchType.FOOTBALL;
+				break;
+			case "Basketball":
+				sportName = MatchType.BASKETBALL;
+				break;
+			case "Snooker":
+				sportName = MatchType.SNOOKER;
+				break;
+			case "Ice Hockey":
+				sportName = MatchType.ICE_HOCKEY;
+				break;
+			case "Volleyball":
+				sportName = MatchType.VOLLEYBALL;
+				break;
+			case "Beach volleyball":
+				sportName = MatchType.VOLLEYBALL_BEACH;
+				break;
+			case "Handball":
+				sportName = MatchType.HANDBALL;
+				break;
+			}
+			sportNameForMatchidMap.put(id, sportName);
+		}	
+		
+		return sportNameForMatchidMap;
+	}
+}
