21a3eac2c7c6e8c3bc10b5bf94540908d6c8795b
Andrey Balakshiy <andreyspbsu94@gmail.com>
2015-10-29 14:11:39 +0300

    added some javadoc

--- Service/src/main/java/KateAndrewService/StatisticOfCodeReader.java
+++ Service/src/main/java/KateAndrewService/StatisticOfCodeReader.java
@@ -14,13 +14,19 @@ import matchstatistic.sportstatistictypes.StatisticType;
 
 /**
  * 
- * @author andrey Class for reading information about what exactly statistic means code of event
+ * @author Andrey Class for reading information about what exactly statistic means code of event
  */
 public class StatisticOfCodeReader
 {
     private Scanner scanner;
 
 
+    /**
+     * Main constructor
+     * 
+     * @param pathToFile Path to .csv file
+     * @throws FileNotFoundException
+     */
     public StatisticOfCodeReader(String pathToFile) throws FileNotFoundException
     {
         scanner = new Scanner(new File(pathToFile));
@@ -39,23 +45,23 @@ public class StatisticOfCodeReader
             String sportName = strings[0];
             int code = Integer.parseInt(strings[1]);
             String statisticTypeString = strings[2];
-            
+
             MatchType sportType = null;
             switch (sportName)
             {
                 case "Soccer":
                     sportType = MatchType.FOOTBALL;
-                    Map <Integer, StatisticType> tempMap = statisticForCodeMap.get(sportType);
-                    if (tempMap == null) {
+                    Map<Integer, StatisticType> tempMap = statisticForCodeMap.get(sportType);
+                    if (tempMap == null)
+                    {
                         tempMap = new HashMap<Integer, StatisticType>();
                     }
                     tempMap.put(code, Football.valueOf(statisticTypeString));
                     statisticForCodeMap.put(sportType, tempMap);
                     break;
             }
-            
-            
-//            System.out.println(sportName + ": code = " + code + " = " + statisticTypeString);
+
+            // System.out.println(sportName + ": code = " + code + " = " + statisticTypeString);
         }
 
         return statisticForCodeMap;
